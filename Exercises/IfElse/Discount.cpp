653f157ab32eb9c32bb163ea299345fe44213321
Alvaro Campos <alvarocampos1563@gmail.com>
2025-04-27 21:28:43 -0600

    Discount Exercise

---
+++ Exercises/IfElse/Discount.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+float price;
+float discount;
+
+    if(price>100){
+
+    cout<<"You have a discount on your purchase of: "<<price<<endl;
+    discount=price*0.10;
+    cout<<"Your new price is"<<price-discount<<endl;
+
+    } else {
+
+        cout<<"Sorry, you do not qualify for the discount."<<endl;
+
+    }
+
+    return 0;
+
+}
