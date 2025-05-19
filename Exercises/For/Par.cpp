65c2dba705235f02cafdfd7b657ac40e30516d45
Alvaro Campos <alvarocampos1563@gmail.com>
2025-05-18 23:32:06 -0600

    Update :memo:

--- Exercises/For/Par.cpp
+++ Exercises/For/Par.cpp
@@ -5,18 +5,18 @@ using namespace std;
 int main()
 {
 
-    int i=0;
-    int contador=0;
+    int i = 0;
+    int contador = 0;
 
-    for ( i = 1; i <= 50; i++)
+    for (i = 1; i <= 50; i++)
     {
-        if (i % 2 == 0){
-        
-            contador++;
-                    cout<<contador<<" : "<<i<<endl;
+        if (i % 2 == 0)
+        {
 
+            contador++;
+            cout << contador << " : " << i << endl;
         }
-        }
+    }
 
     return 0;
 }
