950f2b695717b6134d217cf4bcccf0ba542e1c36
Alvaro Campos <00100725@uca.edu.sv>
2025-06-03 10:05:51 -0600

    update :memo:

--- Exercises/IfElse/UseEnergy.cpp
+++ Exercises/IfElse/UseEnergy.cpp
@@ -13,5 +13,14 @@ int main(){
         cout<<"You need to pay a low tax"<<endl;
     }
 
+    if(kw>=100 && kw<=200){
+
+        cout<<"You need to pay a medium tax"<<endl;
+    }
+
+    if(kw>200){
+
+        cout<<"You need to pay a big tax"<<endl;
+    }
     return 0;
 }
